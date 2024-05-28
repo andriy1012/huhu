838c8b91d5011d0bd8bf55239b6d7c06363f3427
andriy1012 <121753101+andriy1012@users.noreply.github.com>
2024-05-29 00:47:33 +0700

    Update report.js

--- km-feb24-surabaya-7-main/report.js
+++ km-feb24-surabaya-7-main/report.js
@@ -2,20 +2,21 @@ const accordion = document.querySelector('.accordion');
 const items = accordion.querySelectorAll('li');
 
 items.forEach((item) => {
-    const input = item.querySelector('input');
-    const label = item.querySelector('label');
-    const content = item.querySelector('.content');
-  
-    input.addEventListener('click', () => {
-      if (input.checked) {
-        content.style.maxHeight = '400px';
-        content.style.padding = '10px 10px 20px';
-        label.querySelector('::before').textContent = '+';
+  const input = item.querySelector('input[type="radio"]');
+  const label = item.querySelector('label');
+  const content = item.querySelector('.content');
+
+  input.addEventListener('click', () => {
+    items.forEach((otherItem) => {
+      if (otherItem!== item) {
+        otherItem.querySelector('.content').style.maxHeight = '0';
       }
-      else{
-        content.style.maxHeight = '0px';
-        content.style.padding = '0px 0px 0px'; 
-      }
-      
     });
+
+    if (content.style.maxHeight === '0px') {
+      content.style.maxHeight = content.scrollHeight + 'px';
+    } else {
+      content.style.maxHeight = '0';
+    }
   });
+});
